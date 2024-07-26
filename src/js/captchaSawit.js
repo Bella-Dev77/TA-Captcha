edc8b647b8f2f8a8c820d91f3c30992d8f6900cb
Raeza Maulana <raezam85@gmail.com>
2024-07-26 18:45:56 +0700

    revisi lagiii

--- src/js/captchaSawit.js
+++ src/js/captchaSawit.js
@@ -1,18 +1,19 @@
-function showRandomCaptcha() {
-    // Sembunyikan semua captcha
-    $('.captcha').hide();
-
-    // Pilih captcha secara random
-    var randomIndex = Math.floor(Math.random() * 3) + 1;
-    $('#captcha' + randomIndex).show();
-}
-
 $(document).ready(function() {
+    // Fungsi untuk menampilkan captcha secara random
+    function showRandomCaptcha() {
+        // Sembunyikan semua captcha
+        $('#captcha1, #captcha2, #captcha3').hide();
+
+        // Pilih captcha secara random
+        var randomIndex = Math.floor(Math.random() * 3) + 1;
+        $('#captcha' + randomIndex).show();
+    }
+
     // Tampilkan captcha saat halaman pertama kali dimuat
     showRandomCaptcha();
 
     // Ganti captcha ketika tombol refresh diklik
-    $('#refreshCaptcha').click(function() {
+    $('#form').on('click', '#refreshCaptcha', function() {
         showRandomCaptcha();
     });
 });
